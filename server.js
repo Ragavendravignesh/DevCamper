24b53272b1b6d31cf921d393cce0c991f38c6b09
Ragavendra S <ragavendrabest77@gmail.com>
2021-02-24 20:03:36 +0530

    Database connection added

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const logger = require('./middleware/logger');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //To import routes
 const bootcamps = require("./routes/bootcamps");
 
 // To load environnment values
 dotenv.config({ path: "./config/config.env" });
+
+//To connect to database
+connectDB();
 
 const app = express();
 
@@ -22,7 +26,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server is running in ${process.env.NODE_ENV} mode at ${PORT}`)
 );
+
+//To handle unhandled Exceptions
+process.on('unhandledRejection', (err,promise) => {
+  console.log(`Err: ${err.message}`);
+
+  server.close(()=> process.exit(1));
+})
