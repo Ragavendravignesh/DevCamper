45eb3a928f8eddb1d8f61be3c88f2002540de228
Ragavendra S <ragavendrabest77@gmail.com>
2021-03-19 08:46:32 +0530

    Update details and password added

--- controllers/auth.js
+++ controllers/auth.js
@@ -119,6 +119,37 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc Update details
+// @route PUT api/v1/auth/updatedetails
+// @access private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = { name: req.body.name, email: req.body.email };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc Update Password
+// @route PUT api/v1/auth/updatepassword
+// @access private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password did not match', 401));
+  }
+  
+  user.password = req.body.newPassword;
+
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 //It helps to create token and send response
 const sendTokenResponse = (user, statusCode, res) => {
   //create Token
