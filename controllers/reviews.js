4af0b88a4110ba5af66e03177325a6a56d8fee91
Ragavendra S <ragavendrabest77@gmail.com>
2021-03-20 16:45:46 +0530

    Adde review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -31,3 +31,26 @@ exports.getReview = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ succcess: true, data: review });
 });
+
+// @desc Add review
+// @route POST api/v1/reviews/:bootcampId/reviews
+// @access Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if(!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with an id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(200).json({ success: true, data: review });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 
 const router = express.Router({ mergeParams: true });
 
@@ -9,13 +9,16 @@ const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description',
-  }),
-  getReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getReviews
+  )
+  .post(protect, authorize('admin', 'user'), addReview);
 
 router.route('/:id').get(getReview);
 
