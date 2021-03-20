f63b8f558c2cc672e0c77554aef74a1e35b6c491
Ragavendra S <ragavendrabest77@gmail.com>
2021-03-20 16:10:10 +0530

    Modified seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 mongoose.connect(process.env.MONGO_URI, {
   useCreateIndex: true,
@@ -21,14 +22,15 @@ const bootcamps = JSON.parse(
 );
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
-
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`));
 
 const importData = async function () {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log('Data Imported');
     process.exit();
@@ -42,6 +44,7 @@ const deleteData = async function () {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log('Data deleted');
     process.exit();
