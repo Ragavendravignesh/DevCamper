884af414d44d9b99b49aae9f32c4402d5711e343
Ragavendra S <ragavendrabest77@gmail.com>
2021-03-20 16:09:49 +0530

    Added get single user

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -16,3 +16,18 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
   }
 });
+
+// @desc Get single review
+// @route GET api/v1/reviews/:id
+// @access Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review not found with an id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({ succcess: true, data: review });
+});
